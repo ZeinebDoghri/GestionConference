c90cc8a6a446dd45c2971a38d887984d12dd3628
Zeineb Doghri <doghri.zeineb24@gmail.com>
2025-10-13 21:48:01 +0100

    Personalization

--- ConferenceApp/admin.py
+++ ConferenceApp/admin.py
@@ -5,7 +5,7 @@ admin.site.site_header = "Conference Management Admin 25/26"
 admin.site.site_title = "Conference Management Admin Portal"
 admin.site.index_title = "Welcome to Conference Management Admin Portal"
 #admin.site.register(Conference) Register the Conference model to make it accessible in the admin interface
-admin.site.register(Submission)
+#admin.site.register(Submission)
 
 # Tabular inline
 class SubmissionInline(admin.TabularInline):
@@ -18,12 +18,26 @@ class SubmissionInline(admin.TabularInline):
         }),
     )
 
+#Tabular Stacked inline
+#class SubmissionInline(admin.StackedInline):
+#    model = Submission # Model to be displayed inline
+#    extra = 1 # Number of extra forms to display
+#    readonly_fields = ('submission_id', 'submission_date') # Make fields read-only
+#    fieldsets = (
+#        ("General Information", {
+#            'fields': ('submission_id', 'user', 'title', 'abstract', 'status', 'payed')
+#        }),
+#    )
+
+
+
+
 @admin.register(Conference) #Decorator to register the model
 class AdminPersonalized(admin.ModelAdmin):
     list_display = ('name', 'theme', 'location', 'start_date', 'end_date', 'duration') #Add columns to the list view
     ordering = ('-start_date',) #Order by start_date descending
-    search_fields = ('name', 'theme', 'location') #Add a search bar to filter by name, theme, and location
-    list_filter = ('theme', 'location') #Add a filter sidebar to filter by theme and location
+    search_fields = ('name', 'description', 'location') #Add a search bar to filter by name, theme, and location
+    list_filter = ('theme', 'location', 'start_date') #Add a filter sidebar to filter by theme and location
     fieldsets = (
         ("General Information", {
             'fields': ('conference_id', 'name', 'description', 'theme')
@@ -41,4 +55,31 @@ class AdminPersonalized(admin.ModelAdmin):
             return (obj.end_date - obj.start_date).days
         return "RAS"
     duration.short_description = 'Duration (days)' #Change the column header name
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('submission_id', 'title', 'user', 'conference', 'status', 'submission_date', 'payed', 'short_abstract') #Add columns to the list view
+    #Ajouter une méthode personnalisée short_abstract qui tronque l’abstract à 50 caractères pour l’affichage rapide.
+    def short_abstract(self, obj):
+        if obj.abstract:
+            return obj.abstract[:50] + '...' if len(obj.abstract) > 50 else obj.abstract
+        return ''
+    short_abstract.short_description = 'Abstract(short)'
+    list_editable = ('status', 'payed') #Make status and payed editable in the list view
+    ordering = ('-submission_date',) #Order by submission_date descending
+    search_fields = ('title', 'user__username', 'keywords') #Add a search bar to filter by title, user, and conference
+    list_filter = ('status', 'payed', 'submission_date','conference') #Add a filter sidebar to filter by status and payed
+    fieldsets = (
+        ("General Information", {
+            'fields': ('submission_id', 'title', 'abstract', 'keywords')
+        }),
+        ("File and Conference", {
+            'fields': ('paper', 'conference')
+        }),
+        ("Follow-up", {
+            'fields': ('status', 'payed', 'user', 'submission_date')
+        })
+    )
+    readonly_fields = ('submission_id', 'submission_date') #Make submission_id and submission_date read-only
+    date_hierarchy = 'submission_date' #Add a date-based drilldown navigation by submission_date
     
